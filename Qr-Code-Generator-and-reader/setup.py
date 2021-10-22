e2e9c7a193bafd1d9ccb4646366f49729bc3e4f2
Ruban Gino Singh <rubangino22@gmail.com>
2021-10-22 17:30:08 +0530

    Updated setup.py

--- Qr-Code-Generator-and-reader/setup.py
+++ Qr-Code-Generator-and-reader/setup.py
@@ -5,5 +5,15 @@ setup(
     version='1.0',
     author='Ruban Gino Singh',
     author_email='info@rubangino.in',
+    description="Python Application to generate QR code with in one click.",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta", 
+        "Intented Audience :: Social Media managers and Online program managers",
+        "Licence :: OSI Approved :: MIT Licence"
+    ],
+    keywords="QRcode QRgenerator QRreader QRcodeGenerator",
     packages=find_packages(),
+    install_requires=["requests>=2"],
+    python_requires="~=3.5"
 )
