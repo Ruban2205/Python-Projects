80e3b5e1b9a590137188783f5189b8c2f7cbc7d5
Ruban Gino Singh <rubangino22@gmail.com>
2021-10-22 09:36:31 +0530

    QR code Generator and Reader

---
+++ Qr-Code-Generator-and-reader/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QR_Code_Generator_and_Reader',
+    version='1.0',
+    author='Ruban Gino Singh',
+    author_email='info@rubangino.in',
+    packages=find_packages(),
+)
